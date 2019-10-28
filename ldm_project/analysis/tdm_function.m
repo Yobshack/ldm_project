2df3ac0154236e991a00439a03825bf0da8ca6ad
Yobshack <kskuttkakaria@g.harvard.edu>
2019-10-27 23:44:15 -0700

    Update for Cell submission

--- ldm_project/analysis/tdm_function.m
+++
@@ -1,8 +0,0 @@
-function out = tdm_function(x)
-
-corr_map = corr(x,'rows','pairwise','type','Spearman');
-
-net_corr_light = corr_map(3,1) - corr_map(3,2);
-net_corr_dark = corr_map(4,2) - corr_map(4,1);
-
-out = [net_corr_light, net_corr_dark];
